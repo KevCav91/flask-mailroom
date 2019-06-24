b99ed917acfb01f187721b8272c91f554e268add
kevcav91 <kevincavanau@gmail.com>
2019-06-23 18:13:00 -0700

    Lesson 1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,7 +15,20 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/new_donation', methods=['GET', 'POST'])
+def new_donation():
+
+    if request.method == 'POST':
+        d = Donor.select().where(Donor.name == request.form['name']).get()
+        donation = Donation(donor=d, value=request.form['amount'])
+        donation.save()
+
+        return redirect(url_for('all'))
+    else:
+        return render_template('new_donation.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
